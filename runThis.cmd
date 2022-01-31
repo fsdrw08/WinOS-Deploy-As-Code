fbb9f7a480f0990d54c954d3bea1744dde42f966
Windom <drw_08@hotmail.com>
2022-01-31 15:45:39 +0800

    replace choice command to set

--- runThis.cmd
+++ runThis.cmd
@@ -3,15 +3,20 @@
 @REM echo current folder %current%
 
 :CONFIRM
+ECHO Are you confirm to DELETE ALL DATA IN THE COMPUTER DISK?
+SET /p choice=Y or N?
+IF '%choice%'=='Y' GOTO :choice1
+IF '%choice%'=='N' GOTO :choice2
 @REM https://stackoverflow.com/a/11094120/10833894
-CHOICE /T 5 /C YN /D N /M "Are you confirm to DELETE ALL DATA IN THE COMPUTER DISK? "
-GOTO OPTION-%ERRORLEVEL%
+@REM CHOICE /T 5 /C YN /D N /M "Are you confirm to DELETE ALL DATA IN THE COMPUTER DISK? "
+@REM GOTO OPTION-%ERRORLEVEL%
 @REM IF %ERRORLEVEL% ==1 GOTO OPTION-1
 @REM IF %ERRORLEVEL% ==2 GOTO OPTION-2
 
-:OPTION-1
-echo "diskpart.exe /s %~dp0CreatePartitions-UEFI.txt"
-echo "setup.exe /unattend:%~dp0unattend.xml"
-
-:OPTION-2
+:choice1
+diskpart.exe /s %~dp0CreatePartitions-UEFI.txt
+setup.exe /unattend:%~dp0unattend.xml
+:choice2
+EXIT
+:end
 EXIT
