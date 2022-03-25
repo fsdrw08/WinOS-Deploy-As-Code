6c26a0ae8ecd033ba813b1936d97a1cb6e29ca87
Windom <drw_08@hotmail.com>
2022-03-25 16:27:47 +0800

    update language parameter

--- Get-WinISO/Get-Win11ISO.bat
+++ Get-WinISO/Get-Win11ISO.bat
@@ -1,1 +1,1 @@
-powershell -ExecutionPolicy bypass -File "%~dp0\Get-WinISO.ps1"  -WindowsVersion 11 -Language English
+powershell -ExecutionPolicy bypass -File "%~dp0\Get-WinISO.ps1"  -WindowsVersion 11 -Language "English International"
